484644bd0e10d63d89134180fc93502196fd2ea5
sevgi75 <erbilsevgi97@gmail.com>
2024-02-29 18:25:41 +0300

    express get post

--- nodejs/notes/09_ExpressJs-Routing/index.js
+++ nodejs/notes/09_ExpressJs-Routing/index.js
@@ -14,11 +14,21 @@ const express=require("express")
 const app=express() // express üzerinde birserver oluşturduk
 
 app.get('/',(req,res)=>{
-    res.write("hello")
-    res.end()
+    // res.write("hello")
+    // res.end()
+
+    // res.write(JSON.stringify({
+    //     message:"hello"
+    // }))
+    // res.end()
     
     // console.log('hello');
-    // res.send()
+    res.send({  // tercih edilen
+        "message":"hello"
+    })
+    // res.send('hello FS15') // birden fazla send manasız
 })
+
+app.post('/',(req,res)=>{ res.send({ "message":"POST method called" })})
 
 app.listen(PORT,HOST,()=>console.log(`Server runned http://${HOST}:${PORT}`))
