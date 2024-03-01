8a56db275035ff3130bf8de753412ec85a7c7897
sevgi75 <erbilsevgi97@gmail.com>
2024-03-01 09:28:31 +0300

    joker characters

--- nodejs/notes/09_ExpressJs-Routing/index.js
+++ nodejs/notes/09_ExpressJs-Routing/index.js
@@ -13,6 +13,7 @@ const express=require("express")
 
 const app=express() // express üzerinde birserver oluşturduk
 
+/*
 app.get('/',(req,res)=>{
     // res.write("hello")
     // res.end()
@@ -34,5 +35,15 @@ app.get('/',(req,res)=>{
 // app.delete('/',(req,res)=>{ res.send({ "message":"DELETE method called" })})
 
 app.all('/',(req,res)=>{res.send({ "message":"ALL method called"})})
+*/
+// app.get('/drive1/drive2',(req,res)=>{ res.send({ "message":"GET method called" })})
+
+// joker characters
+// app.get('/',(req,res)=>{ res.send({ "message":"GET method called" })})
+// app.get('/ab(x)?12',(req,res)=>{ res.send({ message:"ab12 or abx12" })}) // only ab12 and abx12
+// app.get('/ab(x)+12',(req,res)=>{ res.send({ message:"abx........x12" })}) // ab ve 12 arasına n tane x gelebilir
+
+app.get('/ab*',(req,res)=>{ res.send({ "message":"GET method called" })}) //sonu ne olursa olsun
+app.get('/a*b',(req,res)=>{ res.send({ "message":"GET method called" })}) // başı a sonu b ortada ne olursa olsun
 
 app.listen(PORT,HOST,()=>console.log(`Server runned http://${HOST}:${PORT}`))
