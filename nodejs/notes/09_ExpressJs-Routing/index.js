83c53bfc1d43385d88787b99f70b792de746abbf
sevgi75 <erbilsevgi97@gmail.com>
2024-03-01 10:00:01 +0300

    regex

--- nodejs/notes/09_ExpressJs-Routing/index.js
+++ nodejs/notes/09_ExpressJs-Routing/index.js
@@ -43,7 +43,15 @@ app.all('/',(req,res)=>{res.send({ "message":"ALL method called"})})
 // app.get('/ab(x)?12',(req,res)=>{ res.send({ message:"ab12 or abx12" })}) // only ab12 and abx12
 // app.get('/ab(x)+12',(req,res)=>{ res.send({ message:"abx........x12" })}) // ab ve 12 arasına n tane x gelebilir
 
-app.get('/ab*',(req,res)=>{ res.send({ "message":"GET method called" })}) //sonu ne olursa olsun
-app.get('/a*b',(req,res)=>{ res.send({ "message":"GET method called" })}) // başı a sonu b ortada ne olursa olsun
+// app.get('/ab*',(req,res)=>{ res.send({ "message":"GET method called" })}) //sonu ne olursa olsun
+// app.get('/a*b',(req,res)=>{ res.send({ "message":"GET method called" })}) // başı a sonu b ortada ne olursa olsun
+
+// regex
+//! iki / arasında regex kabul edilir '' kullanmayın
+// regexr.com
+// app.get(/abc$/,(req,res)=>{ res.send({ message:"start with any" })}) //başı ne olursa olsun sonu abc olacak
+// app.get(/^\/abc/,(req,res)=>{ res.send({ message:"and with any" })}) // başı abc olacak sonu ne olursa olsun
+// app.get(/\/*abc/,(req,res)=>{ res.send({ message:"start with any" })}) // başlangıç ne olursa olsun
+app.get(/abc/,(req,res)=>{ res.send({ message:"find abc in path" })}) // içinde abc olacak başına sonuna ne gelirse gelsin
 
 app.listen(PORT,HOST,()=>console.log(`Server runned http://${HOST}:${PORT}`))
