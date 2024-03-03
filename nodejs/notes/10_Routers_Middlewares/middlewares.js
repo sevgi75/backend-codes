f9c114fbacf2db7f0fcb325147fa551ecc9105bc
sevgi75 <erbilsevgi97@gmail.com>
2024-03-03 19:04:35 +0300

    middlewares

--- nodejs/notes/10_Routers_Middlewares/middlewares.js
+++ nodejs/notes/10_Routers_Middlewares/middlewares.js
@@ -9,10 +9,134 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 //? Middleware functions must be has three parameters. 
 //? Last parameter is for next().
 
+// Middleware:
+app.get('/',(req,res,next)=>{
 
+    console.log('Middleware started.');
+
+    // Go to next route:
+    next()
+
+    // console.log('Middleware 24 started.');
+
+    // res.send({
+    //     message: 'Middleware'
+    // })
+})
+
+// Route-Path
+app.get('/',(req,res)=>{
+
+    console.log('Route started.');
+
+    res.send({
+        message: 'Welcome'
+    })
+})
+
+/* ------------------------------------------------------- *
+
+// Middleware:
+app.get('/',(req,res,next)=>{
+
+    if(req.query?.username == 'clarusway') {
+
+        //? Send data by req/res:
+        req.welcome = 'Welcome'
+        res.username = req.query?.username
+
+        next()
+
+    } else {
+
+        res.send({
+            message: 'username is wrong.'
+        })
+
+    }    
+})
+
+// Route-Path
+app.get('/',(req,res)=>{
+
+    res.send({
+        // message: 'Welcome'
+        message: req?.welcome + ' to ' + res?.username
+    })
+})
+/* ------------------------------------------------------- *
+
+app.get('/', (req, res, next) => {
+
+    req.message1 = 'middleware-1 started.'
+    next()
+})
+
+app.get('/', (req, res, next) => {
+
+    req.message2 = 'middleware-2 started.'
+    next()
+})
+
+app.get('/', (req, res, next) => {
+
+    req.message3 = 'middleware-3 started.'
+    next()
+})
+
+app.get('/', (req, res, next) => {
+
+    req.message4 = 'middleware-4 started.'
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.send({
+        message1: req.message1,
+        message2: req.message2,
+        message3: req.message3,
+        message4: req.message4,
+        message: "Finished",
+    })
+})
+
+/* ------------------------------------------------------- */
+
+const middleFunc1 = (req, res, next) => {
+
+    req.message1 = 'middleFunc1 started.'
+    next()
+}
+
+const middleFunc2 = (req, res, next) => {
+
+    req.message2 = 'middleFunc2 started.'
+    next()
+}
+
+// Use middlewares:
+// app.use(middleFunc1)
+// app.use(middleFunc2)
+//? Altarnative:
+// app.use(middleFunc1, middleFunc2)
+//? Altarnative:
+// app.use([middleFunc1, middleFunc2]) // all paths
+//? It can use URL:
+// app.get('/abc', [middleFunc1, middleFunc2]) // only /abc only get
+app.use('/abc', [middleFunc1, middleFunc2]) // only /abc/* and all methods
+
+app.all('/*', (req, res) => {
+    res.send({
+        message1: req.message1,
+        message2: req.message2,
+        message: "Finished",
+    })
+})
+
+/* ------------------------------------------------------- */
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
