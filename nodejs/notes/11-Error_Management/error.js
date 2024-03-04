9b7ce4d551de8b4f0ae7b32f48fe10cd9b0ede1d
sevgi75 <erbilsevgi97@gmail.com>
2024-03-04 13:27:45 +0300

    try catch

--- nodejs/notes/11-Error_Management/error.js
+++ nodejs/notes/11-Error_Management/error.js
@@ -9,10 +9,39 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
-/* ------------------------------------------------------- */
+/* ------------------------------------------------------- *
 app.get('/',(req,res)=>{
-    res.send('Hello')
+    throw new Error('Error Message')
 })
+
+app.get('/user/:id', (req, res) => {
+
+    const id = req.params?.id || 0
+
+    try {
+        if(isNaN(id)) {
+
+            throw new Error('ID is not a number', { cause: 'params.id =' + id })
+    
+        }else{
+    
+            res.send({
+                message: 'OK',
+                id
+            })
+        }
+    }catch(err){
+
+        res.send({
+            error: true,
+            message: err.message
+        })
+
+    }
+
+    
+})
+/* ------------------------------------------------------- */
 
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
