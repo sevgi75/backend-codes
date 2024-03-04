df88ff4f4c8cbecc4fb87ea39a7a783ac81a06cd
sevgi75 <erbilsevgi97@gmail.com>
2024-03-04 13:53:47 +0300

    errorHandler

--- nodejs/notes/11-Error_Management/error.js
+++ nodejs/notes/11-Error_Management/error.js
@@ -42,6 +42,35 @@ app.get('/user/:id', (req, res) => {
     
 })
 /* ------------------------------------------------------- */
+//* ERROR HANDLER
 
+app.get('/*', (req, res) => {
+
+    res.errorStatusCode = 404
+
+    throw new Error('Error Message')
+})
+
+//? errorHandler is middleware and has must be four parametters. (error, request, response, next)
+const errorHandler = (err, req, res, next) => {
+
+    console.log('errorHandler started.');
+
+    const errorStatusCode = res?.errorStatusCode || 500
+
+    res.status(errorStatusCode).send({
+        error: true,
+        message: err.message
+    })
+
+}
+//? for run errorHandler call in use.
+//? It must be at last middleware.
+app.use(errorHandler)
+
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
 /* ------------------------------------------------------- */
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
