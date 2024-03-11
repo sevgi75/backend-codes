f8e165c9fd9ab60819d0a87cde0f525de057a6e0
sevgi75 <erbilsevgi97@gmail.com>
2024-03-11 13:14:48 +0300

    templated

---
+++ nodejs/notes/15-BlogAPI_Mongoose/src/models/blog.model.js
@@ -0,0 +1,42 @@
+"use strict"
+/*
+    BLOG API MODELS
+*/
+
+const mongoose=require("mongoose")
+
+
+
+
+
+
+// const nameSchema= new mongoose.Schema({fields},{tablo adı})
+/*
+const nameSchema= new mongoose.Schema(
+    {
+        //    _id: // auto created and increment
+
+        // fieldName: Type // short form
+        // fieldName: String,
+        // fieldName2: BigInt
+
+        fieldName: {
+            type: String,
+            default: null,
+            trim: true,  
+            unique: true, // benzersiz kayıt
+            select: false, //model çağrıldığında gelsinmi
+            index: false, // aramalarda erişimi hızlandırır
+            required: true, // veri girişi gerklimi
+            required: [true, 'error message'], // gereklimi değilmi , hata mesajı
+            enum:[[1,2,3],'error message'], // belirli bir pattern e göre veri girişi
+            validate: [function(data){ return true},'error message'],// veri fonksiyon ile doğrulama
+            get: function(data){ return data} ,      // veriyi çağırırken çalışacak fonksiyon
+            set: function(data){ return data}       // veriyi kaydederken çalışacak fonksiyon
+        }
+    },
+    {
+        collection:'collectionName', // tablo ismi
+        timestamps: true // createdate, updateDate
+    }
+)*/
