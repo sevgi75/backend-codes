ed76aea16da5feacc9fefe2651861415af920999
sevgi75 <erbilsevgi97@gmail.com>
2024-03-13 12:33:06 +0300

    router added for BlogCategory

--- nodejs/notes/16-BlogAPI_UserModel/src/routes/blog.route.js
+++ nodejs/notes/16-BlogAPI_UserModel/src/routes/blog.route.js
@@ -4,13 +4,22 @@
 /* ====================================================== */
 const router=require("express").Router()
 
-const {BlogPost}=require("../controllers/blog.controller")
+const {BlogCategory, BlogPost}=require("../controllers/blog.controller")
 
+// BlogCategory:
+router.route('/categories')
+    .get(BlogCategory.list)
+    .post(BlogCategory.create)
+router.route('/categories/:categoryId')
+    .get(BlogCategory.read)
+    .put(BlogCategory.update) // put patch aynı
+    .patch(BlogCategory.update) 
+    .delete(BlogCategory.delete)
 
+// BlogPost:
 router.route('/posts')
     .get(BlogPost.list)
     .post(BlogPost.create)
-
 router.route('/posts/:postId')
     .get(BlogPost.read)
     .put(BlogPost.update) // put patch aynı
