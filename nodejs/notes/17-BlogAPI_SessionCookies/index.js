d950bf295640d9f1ba6c5640bafd0806269db26c
sevgi75 <erbilsevgi97@gmail.com>
2024-03-13 21:13:06 +0300

    session cookies

---
+++ nodejs/notes/17-BlogAPI_SessionCookies/index.js
@@ -0,0 +1,33 @@
+"use strict"
+/*
+    BLOG API with Mongoose
+*/
+/*
+    $ npm i express dotenv express-async-errors
+    $ npm i mongoose
+*/
+const express = require("express")
+const app = express()
+
+
+app.use(express.json()) // yukarıda  kalsın
+
+require('dotenv').config()
+const PORT = process.env.PORT
+const HOST = process.env.HOST
+
+/* DB connection  */
+require('./src/configs/dbConnection') // dotenv çalıştıktan sonra 
+
+app.all('/', (req, res) => {
+    res.send('WELCOME BLOG API PROJECT')
+})
+
+app.use('/user', require("./src/routes/user.router"))
+app.use('/blog', require("./src/routes/blog.router"))
+
+app.use(require('./src/middlewares/errorHandler')) // aşağıda kalsın
+
+app.listen(PORT, () => console.log(` Server Running on http://${HOST}:${PORT}`))
+
+// require('./src/sync')()
