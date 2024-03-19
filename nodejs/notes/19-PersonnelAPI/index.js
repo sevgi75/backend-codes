e3a0aa6a2a4d12a48246a3607beeaa39e1f1cbaf
sevgi75 <erbilsevgi97@gmail.com>
2024-03-19 11:52:08 +0300

    personnelApi starter

---
+++ nodejs/notes/19-PersonnelAPI/index.js
@@ -0,0 +1,52 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
+/*
+    $ npm i express dotenv mongoose express-async-errors
+    $ npm i cookie-session
+    $ npm i jsonwebtoken
+*/
+
+const express = require('express')
+const app = express()
+
+/* ------------------------------------------------------- */
+// Required Modules:
+
+//envVariables to process.env
+require('dotenv').config()
+const PORT=process.env?.PORT || 8000
+
+// asyncErrors to errorHandler
+require("express-async-errors")
+
+/* ------------------------------------------------------- */
+// Configurations:
+
+// Connect to DB:
+const { dbConnection } = require('./src/configs/dbConnection')
+dbConnection()
+
+
+
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+/* ------------------------------------------------------- */
+
+
+
+
+
+
+/* ------------------------------------------------------- */
+
+// errorHandler:
+app.use(require('./src/middlewares/errorHandler'))
+
+// RUN SERVER:
+app.listen(PORT, () => console.log('http://127.0.0.1:' + PORT))
+
+/* ------------------------------------------------------- */
+// Syncronization (must be in commentLine):
+// require('./src/helpers/sync')()
