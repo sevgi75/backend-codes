3b08d389927f83a053b2050a597acd42db95d9f9
sevgi75 <erbilsevgi97@gmail.com>
2024-03-20 22:23:28 +0300

    routing

--- nodejs/notes/20-PersonnelAPI_Token_Perm/index.js
+++ nodejs/notes/20-PersonnelAPI_Token_Perm/index.js
@@ -74,12 +74,13 @@ app.all('/', (req, res) => {
     })
 })
 
-//  /departments
-app.use('/departments', require('./src/routes/department.router'))
-// /personnels
-app.use('/personnels', require('./src/routes/personnel.router'))
+// //  /departments
+// app.use('/departments', require('./src/routes/department.router'))
+// // /personnels
+// app.use('/personnels', require('./src/routes/personnel.router'))
 
-
+// app.use(require('./src/routes/index'))
+app.use(require('./src/routes/'))
 /* ------------------------------------------------------- */
 
 // errorHandler:
---
+++ nodejs/notes/20-PersonnelAPI_Token_Perm/src/routes/index.js
@@ -0,0 +1,15 @@
+"use strict"
+/* -------------------------------------------------------
+    EXPRESS - Personnel API
+------------------------------------------------------- */
+const router = require('express').Router()
+/* ------------------------------------------------------- */
+
+// /department:
+router.use('/departments', require('./department.router'))
+// /personnel:
+router.use('/personnels', require('./personnel.router'))
+// /token:
+router.use('/tokens', require('./token.router'))
+
+module.exports = router
