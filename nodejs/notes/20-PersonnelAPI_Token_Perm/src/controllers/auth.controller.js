2a611e9e2dfbff2e83ac7297e726108ee7b29d1a
sevgi75 <erbilsevgi97@gmail.com>
2024-03-21 17:43:04 +0300

    logout token

--- nodejs/notes/20-PersonnelAPI_Token_Perm/src/controllers/auth.controller.js
+++ nodejs/notes/20-PersonnelAPI_Token_Perm/src/controllers/auth.controller.js
@@ -4,11 +4,10 @@
 ------------------------------------------------------- */
 
 const Personnel = require("../models/personnel.model");
-const Token = require('../models/token.model')
-const passwordEncrypt = require('../helpers/passwordEncrypt')
+const Token = require("../models/token.model");
+const passwordEncrypt = require("../helpers/passwordEncrypt");
 
 module.exports = {
- 
   // LOGIN & LOGOUT
 
   login: async (req, res) => {
@@ -33,13 +32,13 @@ module.exports = {
         /* TOKEN */
 
         // Token var mı?
-        let tokenData = await Token.findOne({userId: user._id})
+        let tokenData = await Token.findOne({ userId: user._id });
 
         // Eğer token yoksa oluştur:
         if (!tokenData) {
-          const tokenKey = passwordEncrypt(user._id + Date.now())
+          const tokenKey = passwordEncrypt(user._id + Date.now());
           // console.log(typeof tokenKey, tokenKey);
-          tokenData = await Token.create({userId: user._id, token: tokenKey})
+          tokenData = await Token.create({ userId: user._id, token: tokenKey });
         }
 
         /* TOKEN */
@@ -64,12 +63,31 @@ module.exports = {
     // Set session to null:
     req.session = null;
     /* SESSION */
+
     /* TOKEN */
+    // 1. Yöntem (Kısa yöntem):
+    //? Her kullanıcı için sadece 1 adet token var ise (tüm cihazlardan çıkış yap):
+
+    // console.log(req.user)
+    // const deleted = await Token.deleteOne({ userId: req.user._id })
+
+    // 2. Yöntem:
+    //? Her kullanıcı için 1'den fazla token var ise (çoklu cihaz):
+
+    const auth = req.headers?.authorization || null; // Token ...tokenKey...
+    const tokenKey = auth ? auth.split(" ") : null; // ['Token', '...tokenKey...']
+
+    let deleted = null
+    if (tokenKey && tokenKey[0] == "Token") {
+      deleted = await Token.deleteOne({ token: tokenKey[1] });
+    }
 
     /* TOKEN */
     res.status(200).send({
       error: false,
-      message: "Logout: Sessions Deleted.",
+      // message: "Logout: Sessions Deleted.",
+      message: "Logout: Token Deleted.",
+      deleted
     });
   },
 };
