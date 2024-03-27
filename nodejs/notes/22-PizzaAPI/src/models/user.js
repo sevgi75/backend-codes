4e66e533f49d39fa36abf3e051f7c6a5de808d82
sevgi75 <erbilsevgi97@gmail.com>
2024-03-27 13:20:19 +0300

    user model added

--- nodejs/notes/22-PizzaAPI/src/models/user.js
+++ nodejs/notes/22-PizzaAPI/src/models/user.js
@@ -1,6 +1,51 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+// User Model:
+
+const passwordEnrypt = require("../helpers/passwordEncrypt");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+
+    password: {
+      type: String,
+      trim: true,
+      required: true,
+      set: passwordEnrypt,
+    },
+
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      required: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    collection: "users",
+    timestamps: true,
+  }
+);
+
+// Model:
+module.exports = mongoose.model("User", UserSchema);
