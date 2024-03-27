b88cd095fdaed3f9f5b738dd112936bf1408683d
sevgi75 <erbilsevgi97@gmail.com>
2024-03-27 18:30:14 +0300

    pizza model added

--- nodejs/notes/22-PizzaAPI/src/models/pizza.js
+++ nodejs/notes/22-PizzaAPI/src/models/pizza.js
@@ -1,6 +1,41 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+// Pizza Model:
+
+const PizzaSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    // image: {
+    //     type: String,
+    //     trim: true,
+    // },
+
+    image: String,
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    toppingIds: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Topping",
+      },
+    ],
+  },
+  {
+    collection: "pizzas",
+    timestamps: true,
+  }
+);
