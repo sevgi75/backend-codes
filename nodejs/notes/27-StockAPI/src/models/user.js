9657644a1ade573613bfaf89a50ff702b1cb2be6
sevgi75 <erbilsevgi97@gmail.com>
2024-04-18 15:19:49 +0300

    pre save added

--- nodejs/notes/27-StockAPI/src/models/user.js
+++ nodejs/notes/27-StockAPI/src/models/user.js
@@ -59,6 +59,7 @@ const UserSchema = new mongoose.Schema(
       required: true,
       unique: true,
       index: true,
+      // validate: ... // validasyon işlemini pre(save) yapıyor.
     },
 
     firstName: {
@@ -93,6 +94,61 @@ const UserSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+/* ------------------------------------------------------- */
+// https://mongoosejs.com/docs/middleware.html
+
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+
+// "save" olayı "updateOne" için çalışmaz.
+UserSchema.pre(["save", "updateOne"], function (next) {
+  // console.log('pre(save,update) run.')
+  // console.log(this)
+
+  // get data from "this" or "this._update"
+  const data = this?._update || this;
+
+  // email@domain.com
+  const isEmailValidated = data.email
+    ? /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(data.email)
+    : true;
+
+  if (isEmailValidated) {
+    // console.log('Email OK')
+
+    if (data?.password) {
+      const isPasswordValidated =
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(
+          data.password
+        );
+
+      if (isPasswordValidated) {
+        // console.log('Password OK')
+
+        data.password = passwordEncrypt(data.password);
+
+        if (this?._update) {
+          this._update = data;
+          // this._update.password = data.password
+        } else {
+          // this = data // izin vermiyor.
+          this.password = data.password;
+        }
+
+        //? ShortHand:
+        // // save:
+        // this.password = data.password = passwordEncrypt(data.password)
+        // // update:
+        // this._update = data
+      } else {
+        next(new Error("Password is not validated."));
+      }
+    }
+    next();
+  } else {
+    next(new Error("Email is not validated."));
+  }
+});
 
 /* ------------------------------------------------------- */
 // Exports:
