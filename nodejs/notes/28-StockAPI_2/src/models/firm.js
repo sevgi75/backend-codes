c85a067d71d691d7986d79ecfc517b08e9f4274d
sevgi75 <erbilsevgi97@gmail.com>
2024-04-24 15:36:35 +0300

    firm model added

--- nodejs/notes/28-StockAPI_2/src/models/firm.js
+++ nodejs/notes/28-StockAPI_2/src/models/firm.js
@@ -1,6 +1,31 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+// Firm Model:
+
+const FirmSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    phone: String,
+
+    address: String,
+
+    image: String,
+  },
+  {
+    collection: "firms",
+    timestamps: true,
+  }
+);
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("Firm", FirmSchema);
