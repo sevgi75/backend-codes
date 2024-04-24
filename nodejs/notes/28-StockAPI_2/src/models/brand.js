7ea1d02fc867205aee09d0fd9a7927dc6ecf7e5a
sevgi75 <erbilsevgi97@gmail.com>
2024-04-24 15:18:24 +0300

    brand model added

--- nodejs/notes/28-StockAPI_2/src/models/brand.js
+++ nodejs/notes/28-StockAPI_2/src/models/brand.js
@@ -1,6 +1,30 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- */
+// Brand Model:
+
+const BrandSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+
+    image: {
+      type: String,
+      trim: true,
+    },
+  },
+  {
+    collection: "brands",
+    timestamps: true,
+  }
+);
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model("Brand", BrandSchema);
--- nodejs/notes/28-StockAPI_2/src/routes/category.js
+++ nodejs/notes/28-StockAPI_2/src/routes/category.js
@@ -26,8 +26,8 @@ const permissions = require("../middlewares/permissions");
 
 router
   .route("/(:id)?")
-  .get(permissions.isStaff, category.read)
   .post(permissions.isAdmin, category.create)
+  .get(permissions.isStaff, category.read)
   .put(permissions.isAdmin, category.update)
   .patch(permissions.isAdmin, category.update)
   .delete(permissions.isAdmin, category.delete);
